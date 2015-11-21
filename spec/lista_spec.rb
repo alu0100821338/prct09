0e5505725f6b5fdc38a803e86866b21b326aa91b
alu0100821338 <alu0100821338@ull.edu.es>
2015-11-21 22:11:04 +0000

    Probando max y min lista

--- spec/lista_spec.rb
+++ spec/lista_spec.rb
@@ -128,6 +128,53 @@ describe '# COMPARABLE' do
 end
 
 describe '# ENUMERABLE' do
+     LIBRO1 = Libro::Libro.new()
+      aut01=%w{Dave.Thomas Andy.Hunt Chad.Fowler}
+      Tit01 = "Programming Ruby 1.9 & 2.0: The Pragmatic Programmers’ Guide"
+      Ser01 = "(The Facets of Ruby)"
+      Edt01 = "Pragmatic Bookshelf"
+      Edc01 = "4 edition"
+      fch01 = "(July 7, 2013)"
+      num01 = %w{ ISBN-13:978-1937785499 ISBN-10:1937785491 }
+      
+      LIBRO1.setA(aut01)
+      LIBRO1.setT(Tit01)
+      LIBRO1.setS(Ser01)
+      LIBRO1.setEdt(Edt01)
+      LIBRO1.setEdc(Edc01)
+      LIBRO1.setFecha(fch01)
+      LIBRO1.setNum(num01)
+      LIBRO1.setNRef(100)
+  
+  
+     LIBRO2 = Libro::Libro.new()
+      aut02=%w{Dave.Thomas Andy.Hunt Chad.Fowler}
+      Tit02 = "Programming Ruby 1.9 & 2.0: The Pragmatic Programmers’ Guide"
+      Ser02 = "(The Facets of Ruby)"
+      Edt02 = "Pragmatic Bookshelf"
+      Edc02 = "4 edition"
+      fch02 = "(July 7, 2013)"
+      num02 = %w{ ISBN-13:978-1937785499 ISBN-10:1937785491 }
+      
+      LIBRO2.setA(aut02)
+      LIBRO2.setT(Tit02)
+      LIBRO2.setS(Ser02)
+      LIBRO2.setEdt(Edt02)
+      LIBRO2.setEdc(Edc02)
+      LIBRO2.setFecha(fch02)
+      LIBRO2.setNum(num02)
+      LIBRO2.setNRef(100)
+      
+      
+    it '# Comprobar max y min lista' do
+        L = Lista::Lista.new()
+        L.insert(LIBRO1)
+        L.insert(LIBRO2)
+        
+    
+          expect(L.max).to eq(LIBRO1)
+          expect(L.min).to eq(LIBRO2)
+    end
 end
 
 
